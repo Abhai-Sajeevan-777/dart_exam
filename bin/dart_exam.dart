dedb4cf541c4527106df09f0151482ec147f14e7
Abhai-Sajeevan-777 <abhaisajeevan73@gmail.com>
2025-01-13 12:48:40 +0530

    first commit

---
+++ bin/dart_exam.dart
@@ -0,0 +1,37 @@
+//2.
+void main(){
+  int a =20;
+  for(int i=1;i<=a;i++){
+     int a= (i % 2);
+    print("$a");
+  }
+
+//6.
+
+// int b=4;
+// for(int i=1;i<=b;i++){
+//   for(int j=i;j<=i;j++){
+//    if(j==1){
+//     print("$j");
+//      }else if(j==2){
+//        print("$i $i ");
+//      }else if(j==3){
+//       print("$i $i $i");   
+//      }else if(j>=4){
+//       print("$i $i $i $i");
+//      }
+//  }
+//  }
+
+//}
+
+//5.
+
+// void main(){
+
+
+// }
+// dynamic findlargest(<12,45,7,89,23>){
+//   int largest =
+//   return largest;
+ }
